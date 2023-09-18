4eff63be9d18a1dfd735328593c7df6c42352e00
RobbyRisse <robby.risse@gmail.com>
2023-09-18 10:31:31 +0200

    yo

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const carouselChildrens = [...carousel.children];
-
+// yo
 // Add event listeners for the arrow buttons to scroll the carousel left and right
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
